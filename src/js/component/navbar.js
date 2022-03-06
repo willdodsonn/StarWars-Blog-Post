e210a1de11024006a2687cc46870d891f6910afc
willdodsonn <sodlax38@gmail.com>
2022-03-06 16:49:07 +0000

    first commit

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,16 +2,33 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-dark bg-dark mb-3">
+      <div className="container">
+        <Link to="/">
+          <span className="navbar-brand mb-0 h1">
+            {" "}
+            <div className="col">
+              <div className="logo-image">
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/1280px-Star_Wars_Logo.svg.png"
+                  className="img-fluid"
+                />
+              </div>
+            </div>
+          </span>
+        </Link>
+        <div className="ml-auto">
+		<div className="col">
+          <Link to="/demo">
+		  
+            <button className="btn btn-primary">
+              Check the Context in action
+            </button>
+          </Link>
+		  </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
